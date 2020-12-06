75a8cddc91112a57ead5304fdf94f7f25d7e2313
yamuben <yamubbenjamin@gmail.com>
2020-12-06 16:28:43 +0200

    all needed features except viewing one job

---
+++ app/src/main/java/com/example/hazajobsfinal/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.hazajobsfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button  loginBtn;
+
+    TextView createBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        createBtn = findViewById(R.id.createAccountIdMain);
+        loginBtn = findViewById(R.id.loginIdMain);
+
+        createBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,Signup.class));
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,Dashboard.class));
+            }
+        });
+
+
+
+
+    }
+}
