af28979586e2d994db4c80a8819328004956393b
yamuben <yamubbenjamin@gmail.com>
2020-12-06 21:20:21 +0200

    this branch for juft splashing screen

--- app/src/main/java/com/example/hazajobsfinal/MainActivity.java
+++ app/src/main/java/com/example/hazajobsfinal/MainActivity.java
@@ -4,18 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button  loginBtn;
-
+private static int Splash_Time = 3000;
     TextView createBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
         createBtn = findViewById(R.id.createAccountIdMain);
         loginBtn = findViewById(R.id.loginIdMain);
 
